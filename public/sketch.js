07c64d10293591c0aa70c97d0d134a7e50762fd3
bevchou <31548243+bevchou@users.noreply.github.com>
2017-12-06 20:34:46 -0500

    creating p5 messaging system
    creating JSON files of messages,
    basic interface in p5 canvas

--- public/sketch.js
+++ public/sketch.js
@@ -1,32 +1,81 @@
 //GLOBAL VARIABLES
+let canvas;
+let nameInput, msgInput;
+let sendButton;
 let socket;
+let urUsername;
+let msgCount = 0;
+let time;
+
+//JSON object
+let convo = {};
+
 
 function setup() {
-  createCanvas(400, 400);
-  background(0);
+  time = millis();
+  canvas = createCanvas(350, 600);
+  canvas.id('canvas');
+  background(0);;
   noStroke();
-
+  // input
+  nameInput = createInput('');
+  nameInput.size(120, 18);
+  nameInput.position(370, 500);
+  nameInput.changed(updateName);
+  msgInput = createInput('');
+  msgInput.size(200, 70);
+  msgInput.position(25, 640);
+  msgInput.changed(newText);
+  //button
+  sendButton = createButton('send');
+  sendButton.position(300, 650);
+  // socket io script
   socket = io.connect("http://localhost:3000");
-  socket.on('mouse', newDrawing);
+  // socket.on('mouse', newDrawing);
 }
 
+
+function updateName() {
+  urUsername = nameInput.value();
+  nameInput.hide();
+}
+
+function newText() {
+  let message = msgInput.value();
+  let newMsgData = {
+    time: millis(),
+    msg: message,
+    length: message.length,
+    user: urUsername
+  }
+  convo[msgCount] = newMsgData;
+  console.log(convo);
+  msgCount++;
+  return false;
+}
+
+//Should I just make everything dom elements? lol whatver
 
 function draw() {
+  rect(0, 0, width, height);
+  stroke(0);
+  noFill();
+  strokeWeight(2);
 
 }
 
-function newDrawing(data) {
-  fill(0, 0, 255);
-  ellipse(data.x, data.y, 20);
-}
-
-function mouseDragged() {
-  fill(255, 0, 0);
-  ellipse(mouseX, mouseY, 20);
-  console.log("Sending: " + mouseX + ", " + mouseY);
-  let data = {
-    x: mouseX,
-    y: mouseY
-  }
-  socket.emit('mouse', data);
-}
+// function newDrawing(data) {
+//   fill(0, 0, 255);
+//   ellipse(data.x, data.y, 20);
+// }
+//
+// function updateText() {
+//   fill(255, 0, 0);
+//   ellipse(mouseX, mouseY, 20);
+//   console.log("Sending: " + mouseX + ", " + mouseY);
+//   let data = {
+//     x: mouseX,
+//     y: mouseY
+//   }
+//   socket.emit('mouse', data);
+// }
--- server.js
+++ server.js
@@ -19,3 +19,23 @@ function newConnection(socket) {
     console.log(data);
   });
 }
+
+
+// let app = require('express')();
+// let http = require('http').Server(app);
+// let io = require('socket.io')(http);
+//
+// app.get('/', function(req, res) {
+//   res.sendFile(__dirname + '/public/index.html');
+// });
+//
+// io.on('connection', function(socket) {
+//   socket.on('chat message', function(msg) {
+//     io.emit('chat message', msg);
+//   });
+// });
+//
+//
+// http.listen(3000, function() {
+//   console.log('listening on *:3000');
+// });
